1d9e11d7528fc014179a8620e0fcb90a7dcbe958
captainHayek <49540063+captainHayek@users.noreply.github.com>
2019-05-10 14:13:05 +1000

    Update UN-ComTrade-API-Main-Python v3.6.py
    Added list of country codes and linked Harmonised System codes to Australian Bureau of Statistics codes contained in the CSV file "ABS_6_Digit_HScodes.csv"

--- UN-ComTrade-API-Main-Python v3.6.py
+++ UN-ComTrade-API-Main-Python v3.6.py
@@ -89,7 +89,18 @@ proxies = {"https":"webproxy.agdaff.gov.au:8080"}
 
 # "http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2013&r=826&p=0&rg=all&cc=ALL&fmt=csv"
 # "http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2017&r=598&p=36&rg=all&cc=ALL&fmt=json"
-
+country = [4, 8, 12, 20, 24, 28, 31, 32, 36, 40, 44, 48, 50, 51, 52, 56, 58, 60, 64, 68, 70, 72, 76, 84, 90, 92, 96, 97,
+           100, 104, 108, 112, 116, 120, 124, 132, 136, 140, 144, 148, 152, 156, 170, 174, 175, 178, 180, 184, 188, 191,
+           192, 196, 200, 203, 204, 208, 212, 214, 218, 222, 226, 230, 231, 232, 233, 234, 238, 242, 246, 251, 254, 258,
+           262, 266, 268, 270, 275, 276, 278, 280, 288, 292, 296, 300, 304, 308, 312, 320, 324, 328, 332, 336, 340, 344,
+           348, 352, 356, 360, 364, 368, 372, 376, 381, 384, 388, 392, 398, 400, 404, 408, 410, 414, 417, 418, 422, 426,
+           428, 430, 434, 440, 442, 446, 450, 454, 457, 458, 459, 461, 462, 466, 470, 474, 478, 480, 484, 490, 496, 498,
+           499, 500, 504, 508, 512, 516, 524, 528, 530, 531, 532, 533, 534, 535, 540, 548, 554, 558, 562, 566, 579, 580,
+           582, 583, 584, 585, 586, 588, 590, 591, 592, 598, 600, 604, 608, 616, 620, 624, 626, 634, 638, 642, 643, 646,
+           647, 654, 658, 659, 660, 662, 666, 670, 674, 678, 682, 686, 688, 690, 694, 699, 702, 703, 704, 705, 706, 710,
+           711, 716, 717, 720, 724, 728, 729, 736, 740, 748, 752, 757, 760, 762, 764, 768, 772, 776, 780, 784, 788, 792,
+           795, 796, 798, 800, 804, 807, 810, 818, 826, 834, 835, 836, 841, 842, 850, 854, 858, 860, 862, 866, 868, 876,
+           882, 886, 887, 890, 891, 894]
     
 citrusCodes = ['080510','080520','080530','080540','080550','080590']
 
@@ -101,13 +112,13 @@ url2 = "https://comtrade.un.org/api/get?max=50000&type=C&freq=M&px=HS&ps=2017,20
 #url2 = "http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2017&r=598&p=36&rg=all&cc="
 url3 = "&fmt=json"
 
-hsCodesFile = r"\\act001cl04fs08\piaphdata$\Plant_Health_Policy\Nat_Prog\Surveillance\International Surveillance\Data\DistributionDataScript\harmonisedCodes6D.csv"
+hsCodesFile = r"\\act001cl04fs08\piaphdata$\Plant_Health_Policy\Nat_Prog\Surveillance\International Surveillance\Data\DistributionDataScript\ABS_6_Digit_HScodes.csv"
 hsCodesFrame = pd.read_csv(hsCodesFile, low_memory=False, na_filter=False, encoding="cp1252")
 blankUNtradefile = r"\\act001cl04fs08\piaphdata$\Plant_Health_Policy\Nat_Prog\Surveillance\International Surveillance\Data\DistributionDataScript\comtradeMain.csv"
 csvMainUNdata = pd.read_csv(blankUNtradefile, low_memory=False, na_filter=False)
 
-for code in range(0,len(citrusCodes)):
-    urlUn = url2 + str(citrusCodes[code]) + url3
+for code in range(0,len(hsCodesFrame['HS Code Label'])):
+    urlUn = url2 + str(hsCodesFrame['HS Code Label'][code]) + url3
     print(urlUn)
     #UNresponse = json.loads(requests.get(urlUn,proxies=proxies).text)
     pause()
